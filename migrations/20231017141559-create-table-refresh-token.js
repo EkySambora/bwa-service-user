5090f68a380cb7f062c0db33070eb803d36c0142
ekysambora <ekysambora@gmail.com>
2023-10-17 21:33:37 +0700

    add: create refresh token table

--- migrations/20231017141559-create-table-refresh-token.js
+++ migrations/20231017141559-create-table-refresh-token.js
@@ -9,6 +9,41 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+
+    await queryInterface.createTable('refresh-tokens', {    
+      id: {
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true,
+        allowNull:false
+      },
+      token:{
+        type: Sequelize.TEXT,
+        allowNull:false
+      },
+      user_id:{
+        type: Sequelize.INTEGER,
+        allowNull:false,
+      },
+      created_at:{
+        type: Sequelize.DATE,
+        allowNull:false
+      },
+      updated_at:{
+        type: Sequelize.DATE,
+        allowNull:false
+      }
+    });
+
+    await queryInterface.addConstraint("refresh-tokens",{
+      type: "foreign key",
+      name:"REFRESH_TOKENS_USER_ID",
+      fields:["user_id"],
+      references:{
+        table: "users",
+        field:"id"
+      }
+    })
   },
 
   async down (queryInterface, Sequelize) {
@@ -16,7 +51,9 @@ module.exports = {
      * Add reverting commands here.
      *
      * Example:
-     * await queryInterface.dropTable('users');
+     * await queryInterface.dropTable('refresh-tokens');
      */
+
+    await queryInterface.dropTable('refresh-tokens');
   }
 };
