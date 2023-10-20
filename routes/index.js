ee991d33a8cd536f1a3d157a92c62c8e93dfec28
ekysambora <ekysambora@gmail.com>
2023-10-21 05:18:23 +0700

    feat: install jsonwebtoken

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "bwamicro23";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function async(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
